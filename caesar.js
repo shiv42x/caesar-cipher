00036bdac636bc1a943d4b3e39477461bbb9aea8
shiv42x <shivam2.patel@ryerson.ca>
2021-05-31 14:16:06 -0400

    initial commit

---
+++ caesar.js
@@ -0,0 +1,39 @@
+const caesar = function(str, shift) {
+    let cipher = "";
+    let shift2 = Math.abs(shift) % 26;
+    for (let i = 0; i < str.length; i++)  {
+        let unicode = str.charCodeAt(i); 
+        if (unicode >= 65 && unicode <= 90) { 
+            if (shift < 0) {
+                unicode -= shift2;
+            }
+            if (shift > 0) {
+                unicode += shift2; 
+            }
+            if (unicode < 65) { 
+                unicode = 90 + 1 - (65 - unicode); 
+            }
+            if (unicode > 90) { 
+                unicode = 65 - 1 + (unicode - 90); 
+            }
+        }
+        if (unicode >= 97 && unicode <= 122) { 
+            if (shift < 0) {
+                unicode -= shift2;
+            }
+            if (shift > 0) {
+                unicode += shift2; 
+            }
+            if (unicode < 97) { 
+                unicode = 122 + 1 - (97 - unicode); 
+            }
+            if (unicode > 122) { 
+                unicode = 97 - 1 + (unicode - 122); 
+            }
+        }
+        cipher += String.fromCharCode(unicode);
+    }
+    return cipher;
+}
+
+module.exports = caesar;
